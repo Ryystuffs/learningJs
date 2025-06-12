3e74be832c596b1f1b48803dcdbb89278cf346a9
Ryan Trinidad <trinidadryan113.gmail.com>
2025-06-12 13:01:49 +0800

    third day first commit

---
+++ thirdDay/third.js
@@ -0,0 +1,42 @@
+//constructors
+/*
+function Car (make, model, year,){
+    this.make = make,
+    this.model = model,
+    this.year = year
+};
+
+const Car1 = new Car("BMW","M3",2005);
+console.log(Car1.make);
+*/
+/*
+class Product {
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+
+    }
+    displayProduct(){
+        console.log(`Procuct: ${this.name}`);
+        console.log(`Price: ${this.price.toFixed(2)}`);
+    }
+    calculateTotal(salesTax){
+        let total = (this.price + (this.price * salesTax)).toFixed(2);
+        return total;
+    }
+}
+
+const salesTax = 0.05;
+const product1 = new Product("Shirt", 19.99);
+const product2 = new Product("Pants", 22.50);
+const product3 = new Product("Underwear", 100.00);
+
+product1.displayProduct();
+product2.displayProduct();
+
+
+const total = product1.calculateTotal(salesTax);
+console.log(`Total price (with tax): ${total}`);
+*/
+
+ 
